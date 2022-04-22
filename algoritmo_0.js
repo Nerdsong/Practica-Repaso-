bb0ab345017bbd784015679d42eab10a8b463b3a
practica centro <cualquiercosa@hotmail.com>
2022-04-22 12:04:37 -0300

    agregué archivos desde el aula

---
+++ algoritmo_0.js
@@ -0,0 +1,63 @@
+// Inicializo variables de mi algoritmo
+var cantBananas = 2;
+var string = "tapar la licuadra";
+var cantHielos = 8;
+var seParoLaLicuadora = true
+var grDeAzucar = 50.01
+
+//  Problema 1
+var manzanasMaria = 5; 
+var manzanasPedro = 8;
+var manzanasSusana = 9;
+var totalManzanas = manzanasMaria + manzanasPedro + manzanasSusana; 
+
+// Problema 2
+
+var magdalenaJuan = 28;
+var maddalenaAmigos = 12;
+var cantManzanasn = 0;
+
+// Problema 3
+
+var hamburguesa = 35;
+var refresco = 18;
+var totalDinero = hamburguesa + refresco;
+
+// Problema 4
+
+var cantgalletas = 5;
+var cantCajas = 4;
+var totalGalletas = cantgalletas * cantCajas;
+
+// Problema 5
+
+var distanciaToledo = 2.6; // Kilometros
+var distanciaCasaTio = 691; // metros
+var distanciaTotalMts= (distanciaToledo * 1000) + distanciaCasaTio;
+var distanciaTotalMilimetros = distanciaTotalMts * 1000;
+
+
+document.write(`
+
+    
+    <p style = "white-space: pre";>
+        -Agarrar la licuadora, 1 vaso, un cuchillo y una tabla
+        -Apartar 2 bananas, medio litro de leche, 50.01 gr de azucar y una cubetera de hielo
+        -Agarrar la licuadora y conectarla al enchufe
+        -Colocar 8 hielos adentro de la licuadora
+        -Agarrar el cuchillo y la tabla; cortar las 2 bananas
+        -Poner los trozos de banana en la licuadora
+        -Verter los 50.01gr de azucar dentro de la licuadora
+        -Volcar el medio litro de leche en la licuadora
+        -Tapar la licuadora
+        -Prender la licuadora
+        -Licuar 2 minutos la mezcla
+        -Parar la licuadora
+        -Sacar la tapa
+        -Agarrar el vaso
+        -Verter la mezcla dentro del vaso
+        -Desenchufar la licuadora
+    
+    </P>
+       
+`)
